a26b557942da9f91753d74c5a158d5b4c2741198
Polina Astrashenok <astrashenok225@gmail.com>
2026-02-25 15:30:20 +0200

    Fix wrong server catch

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,15 +1,17 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import orderRoutes from './routes/order_routes';
+import cors from 'cors';
 
 dotenv.config();
 
 const app = express();
+app.use(cors());
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
-app.use('/api', orderRoutes);
+app.use('/', orderRoutes);
 
 app.listen(PORT, () => {
   console.log(`PostgreSQL connected. OpenAI Tax Resolution active.`);
